ae1781dfc29a186a93171c8b868d19678cd6799b
samzorps <63939307+samzorps@users.noreply.github.com>
2021-12-03 15:16:42 -0500

    added search fragment (currently named TransactionFragment)

--- app/src/main/java/com/example/testmichelle/fragments/TransactionFragment.java
+++ app/src/main/java/com/example/testmichelle/fragments/TransactionFragment.java
@@ -1,5 +1,6 @@
 package com.example.testmichelle.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.testmichelle.R;
+
+import java.util.HashMap;
+import java.util.Hashtable;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +31,8 @@ public class TransactionFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    EditText edtStock;
 
     public TransactionFragment() {
         // Required empty public constructor
@@ -55,12 +63,27 @@ public class TransactionFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transaction, container, false);
+        View view = inflater.inflate(R.layout.fragment_transaction, container, false);
+        edtStock = view.findViewById(R.id.edt_Stock);
+        return view;
+    }
+
+    public void search(View view) {
+        String ticker = edtStock.getText().toString();
+        if (!ticker.equals("")) {
+            YahooFinance.requestSummary(ticker, getContext(), this);
+            YahooFinance.requestSearchFragmentSpark(ticker, "5d", "1d", getContext(), this);
+        }
+    }
+
+    public void displayData(Hashtable<String, String> data){
+
     }
 }
