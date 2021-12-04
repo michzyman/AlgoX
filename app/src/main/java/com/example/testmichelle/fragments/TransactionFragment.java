c649f7c9c316085d813b77682f3e65bd383d2867
samzorps <63939307+samzorps@users.noreply.github.com>
2021-12-04 14:11:14 -0500

    finished search fragment (transaction_fragment)

--- app/src/main/java/com/example/testmichelle/fragments/TransactionFragment.java
+++ app/src/main/java/com/example/testmichelle/fragments/TransactionFragment.java
@@ -1,19 +1,21 @@
 package com.example.testmichelle.fragments;
 
-import android.content.Context;
+import static java.util.Collections.list;
+
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.testmichelle.R;
 
-import java.util.HashMap;
 import java.util.Hashtable;
 
 /**
@@ -32,7 +34,14 @@ public class TransactionFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    Button btnSearch;
     EditText edtStock;
+    TextView txtEsg;
+    TextView txtKeyStatistics;
+    TextView txtFinancialData1;
+    TextView txtFinancialData2;
+
+
 
     public TransactionFragment() {
         // Required empty public constructor
@@ -71,12 +80,30 @@ public class TransactionFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_transaction, container, false);
-        edtStock = view.findViewById(R.id.edt_Stock);
+        edtStock = (EditText) view.findViewById(R.id.edt_Stock);
+        txtEsg = (TextView)view.findViewById(R.id.txtEsg);
+        txtKeyStatistics = (TextView) view.findViewById(R.id.txtKeyStatistics);
+        txtFinancialData1 = (TextView) view.findViewById(R.id.txtFinancialData1);
+        txtFinancialData2 = (TextView) view.findViewById(R.id.txtFinancialData2);
+        btnSearch = (Button) view.findViewById(R.id.btn_search);
+        btnSearch.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("onClickCalled", "yay");
+                search(v);
+            }
+        });
+
+        txtEsg.setText(getString(R.string.esgData, "-", "-", "-", "-", "-", "-"));
+        txtKeyStatistics.setText(getString(R.string.keyStatistics, "-", "-", "-", "-", "-", "-"));
+        txtFinancialData1.setText(getString(R.string.financialData1, "-", "-", "-"));
+        txtFinancialData2.setText(getString(R.string.financialData2, "-", "-", "-"));
         return view;
     }
 
     public void search(View view) {
+        Log.d("searchCalled", "yay");
         String ticker = edtStock.getText().toString();
+        Log.d("tickerGotten", ticker);
         if (!ticker.equals("")) {
             YahooFinance.requestSummary(ticker, getContext(), this);
             YahooFinance.requestSearchFragmentSpark(ticker, "5d", "1d", getContext(), this);
@@ -84,6 +111,19 @@ public class TransactionFragment extends Fragment {
     }
 
     public void displayData(Hashtable<String, String> data){
+
+        for (String key : data.keySet()){
+            if (data.get(key).equals("null") || data.get(key).equals(null)){
+                data.put(key, "-");
+            }
+        }
+
+        txtEsg.setText(getString(R.string.esgData, data.get("esgRaw"), data.get("esgPercentile"), data.get("esgPerformance"),
+                data.get("environmentScore"), data.get("socialScore"), data.get("governanceScore")));
+        txtKeyStatistics.setText(getString(R.string.keyStatistics, data.get("marketCap"), data.get("forwardPE"), data.get("profitMargin"),
+                data.get("priceBook"), data.get("trailingEPS"), data.get("pegRatio")));
+        txtFinancialData1.setText(getString(R.string.financialData1, data.get("currentPrice"), data.get("recommendationKey"), data.get("numberOfAnalystOpinions")));
+        txtFinancialData2.setText(getString(R.string.financialData2, data.get("ebitda"), data.get("earningsGrowth"), data.get("revenueGrowth")));
 
     }
 }
