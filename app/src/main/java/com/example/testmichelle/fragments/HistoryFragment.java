9dc58c7751bc9566238a58eae5af5d7a7272e851
michellezyman <michzyman@gmail.com>
2021-12-04 19:09:55 -0500

    code 4 shelli

--- app/src/main/java/com/example/testmichelle/fragments/HistoryFragment.java
+++ app/src/main/java/com/example/testmichelle/fragments/HistoryFragment.java
@@ -2,6 +2,7 @@ package com.example.testmichelle.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,9 +10,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.testmichelle.R;
+import com.example.testmichelle.model.Algorithm;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class HistoryFragment extends Fragment {
@@ -26,7 +35,25 @@ public class HistoryFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_history, container, false);
-
+        getUpdatedAlgo();
         return view;
+    }
+    public void getUpdatedAlgo(){
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Registered Users");
+        databaseReference.child(firebaseUser.getUid()).child("Algorithm").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnap : snapshot.getChildren()) {
+                    Algorithm algorithm = dataSnap.getValue(Algorithm.class);
+                    System.out.println("this is my algorithm: " + algorithm.stockname);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
