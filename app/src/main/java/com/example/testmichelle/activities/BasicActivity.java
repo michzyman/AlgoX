05508dbb2026cd34eb3e846c4a9c9c62fae7a9b4
michellezyman <michzyman@gmail.com>
2021-12-06 22:44:28 -0500

    "fixed some xml files "

--- app/src/main/java/com/example/testmichelle/activities/BasicActivity.java
+++ app/src/main/java/com/example/testmichelle/activities/BasicActivity.java
@@ -6,10 +6,8 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import com.example.testmichelle.R;
@@ -17,18 +15,14 @@ import com.example.testmichelle.fragments.AccountFragment;
 import com.example.testmichelle.fragments.DisplayBackTestingResults;
 import com.example.testmichelle.fragments.HistoryFragment;
 import com.example.testmichelle.fragments.HomeFragment;
-import com.example.testmichelle.fragments.MoreInfoFragment;
 import com.example.testmichelle.fragments.TechnicalAnalysis;
 import com.example.testmichelle.fragments.TransactionFragment;
 import com.example.testmichelle.fragments.YahooFinance;
 import com.example.testmichelle.fragments.backTestingFragment;
 import com.example.testmichelle.model.Algorithm;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -36,22 +30,16 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.jakewharton.threetenabp.AndroidThreeTen;
 
-import org.ta4j.core.Trade;
 import org.threeten.bp.ZonedDateTime;
 
 import org.ta4j.core.Rule;
 import org.ta4j.core.TimeSeries;
 import org.ta4j.core.TradingRecord;
-import org.threeten.bp.LocalDateTime;
-import org.threeten.bp.ZonedDateTime;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 public class BasicActivity extends AppCompatActivity implements FragmentListener {
     FirebaseUser firebaseUser;
@@ -66,7 +54,7 @@ public class BasicActivity extends AppCompatActivity implements FragmentListener
     public static HashMap<String, ArrayList<Object>> algorithmsRan = new HashMap<String, ArrayList<Object>>();
 
     private DisplayBackTestingResults backTestingResults;
-    private MoreInfoFragment moreInfoFragment;
+//    private MoreInfoFragment moreInfoFragment;
     private backTestingFragment backTestingFragment;
     private HomeFragment homeFragment;
     private TransactionFragment transactionFragment;
@@ -91,7 +79,7 @@ public class BasicActivity extends AppCompatActivity implements FragmentListener
 
         backTestingFragment = new backTestingFragment();
         backTestingResults = new DisplayBackTestingResults();
-        moreInfoFragment = new MoreInfoFragment();
+        //moreInfoFragment = new MoreInfoFragment();
 
         loadingFragment = new LoadingScreenFragment();
         makeCurrentFragment(loadingFragment);
@@ -208,18 +196,16 @@ public class BasicActivity extends AppCompatActivity implements FragmentListener
         backTestingResults.setResultsData();
     }
 
+    /*
     public void goToMoreInfoFragment() {
         makeCurrentFragment(moreInfoFragment);
     }
-
+*/
     public void goToBackTestingFragment() {
         makeCurrentFragment(backTestingFragment);
     }
 
-    @Override
-    public void goToHome() {
-        makeCurrentFragment(homeFragment);
-    }
+
 
     public void goToHistory() {
         makeCurrentFragment(historyFragment);
